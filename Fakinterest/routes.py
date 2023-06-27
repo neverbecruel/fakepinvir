ad7f77035af926d1f4e63e62eaecba6e7f5c8513
João Victor <pipoca.play19@gmail.com>
2023-06-27 01:13:30 -0300

    Authentications updates

--- Fakinterest/routes.py
+++ Fakinterest/routes.py
@@ -5,31 +5,40 @@ from Fakinterest import app, database, bcrypt
 from flask_login import login_required, login_user, logout_user, current_user
 from Fakinterest.forms import FormLogin, FormCriarConta, FormFotos
 from Fakinterest.models import Usuarios, Foto
-from werkzeug.utils import secure_filename
-import os
+import json
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home_page():
     formlogin = FormLogin()
+    global existe
+    existe = True
     if formlogin.validate_on_submit():
         usuario = Usuarios.query.filter_by(email=formlogin.email.data).first()
         if usuario and bcrypt.check_password_hash(usuario.senha, formlogin.senha.data):
             login_user(usuario)
             return redirect(url_for('feed'))
-    return render_template(r'homepage.html', form=formlogin )
+        else:
+            existe = False
+            return redirect(url_for('home_page'))
+    return render_template(r'homepage.html', form=[formlogin, existe])
+
 
 @app.route("/criar-conta", methods=['GET', 'POST'])
 def criar_conta():
     form_criarconta = FormCriarConta()
     if form_criarconta.validate_on_submit():
         senha = bcrypt.generate_password_hash(str(form_criarconta.senha.data))
-        usuario = Usuarios(username=form_criarconta.username.data, senha=senha,
+        usuario = Usuarios.query.filter_by(email=form_criarconta.email.data).first()
+        if not usuario:
+            usuario = Usuarios(username=form_criarconta.username.data, senha=senha,
                            email=form_criarconta.email.data)
-        database.session.add(usuario)
-        database.session.commit()
-        login_user(usuario, remember=True)
-        return redirect(url_for('feed'))
+            database.session.add(usuario)
+            database.session.commit()
+            login_user(usuario, remember=True)
+            return redirect(url_for('feed'))
+        else:
+            return render_template(r'criarconta.html', form=form_criarconta)
     return render_template('criarconta.html', form=form_criarconta)
 
 
@@ -40,15 +49,12 @@ def perfil(id_usuario):
         formFoto = FormFotos()
         if formFoto.validate_on_submit():
             foto = formFoto.foto.data
-            #nome_seguro = secure_filename(foto.filename)
-            '''path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                    app.config["UPLOAD_FOLDER"], nome_seguro)'''
             upload = cloudinary.uploader.upload(foto, format='jpg')
-            #foto.save(path)
-            foto = Foto( id_usuario=current_user.id, url_img=upload['url'] )  #imagem=nome_seguro,
+
+            foto = Foto(id_usuario=current_user.id, url_img=upload['url'])
             database.session.add(foto)
             database.session.commit()
-        return render_template('perfil.html', usuario=current_user, form=formFoto )
+        return render_template('perfil.html', usuario=current_user, form=formFoto)
     else:
         usuario = Usuarios.query.get(int(id_usuario))
         return render_template('perfil.html', usuario=usuario, form=None)
@@ -60,9 +66,9 @@ def logout():
     logout_user()
     return redirect(url_for('home_page'))
 
+
 @app.route('/feed')
 @login_required
 def feed():
     fotos = Foto.query.order_by(Foto.data_criacao.desc()).all()
     return render_template('feed.html', fotos=fotos)
-
