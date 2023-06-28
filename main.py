89de80d569ab0afdecdfdce3ff52809c4f15eb31
João Victor <pipoca.play19@gmail.com>
2023-06-28 10:44:01 -0300

    updates

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from Fakinterest import app
 from Fakinterest import database, app
 from Fakinterest.models import Usuarios, Foto
 import logging
-from time import sleep
 
 
 with app.app_context():
@@ -11,10 +10,9 @@ with app.app_context():
 if __name__ == '__main__':
     app.run(debug=True)
 
-logger = logging.getLogger(__name__)
-while True:
-    logging.debug('Keeping server running...')
-    sleep(10)
+
+
+
 
 
 
